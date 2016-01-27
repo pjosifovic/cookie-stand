d69955c13a02df4b2c42cb793e51fc1bef9e7af8
Predrag Josifovic <pjosifovic@yahoo.com>
2016-01-27 10:30:01 -0800

    adding watch task

--- gruntfile.js
+++ gruntfile.js
@@ -3,11 +3,14 @@ module.exports = function(grunt) {
 // Configure task(s)
 grunt.initConfig({
   pkg: grunt.file.readJSON('package.json'),
+
+  //Compressing js files for FINAl grunt build
   uglify: {
     build: {
       src: 'js/*.js',
       dest: 'js/script.min.js'
     },
+    //Compressing js files during dev
     dev: {
       options: {
         beaitify: true,
@@ -19,12 +22,23 @@ grunt.initConfig({
       dest: 'js/script.min.js'
     }
   }
+
+  // Watch tasks
+  watch: {
+    js: {
+      files: ['js/*.js'],
+      tasks: [uglify:dev]
+    }
+  }
+
+
 });
 
 // Load the plugins
 grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-watch');
 
 // Register task(s)
-grunt.registerTask('default',['uglify:dev', ]);
+grunt.registerTask('default',['uglify:dev', 'watch', ]);
 grunt.registerTask('build',['uglify:build', ]);
 };
